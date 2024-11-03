0b80636aa0db98b610885e8d6a72aaae3cd44e66
Saadman Sayeed <786saadman@gmail.com>
2024-11-03 16:09:59 +0600

    Update culture.ts

--- src/utils/culture.ts
+++ src/utils/culture.ts
@@ -30,6 +30,26 @@ export const cultureData:Array<cultureDataType> =[
            
           ]
     },
+        {
+        country:"Kazakasthan",
+        code:"KZ",
+        culture:[
+            { key: "Centralized decision-making", value: "High" },
+            { key: "Managers’ influence", value: "High" },
+            { key: "Deference", value: "High" },
+            { key: "Establish trust", value: "High" },
+            { key: "Solving conflicts by favoritism", value: "Moderate" },
+            { key: "Avoiding conflicts", value: "Moderate" },
+            { key: "Hierarchical structure", value: "Moderate" },
+            { key: "Punctuality", value: "Low" },
+            { key: "Building relationships", value: "High" },
+            { key: "Safeguard workmates’ jobs", value: "Low" },
+            { key: "Communication context", value: "High" },
+            { key: "Coordination and collaboration/Team work", value: "Moderate" },
+            { key: "Gender preference", value: "High" },
+           
+          ]
+    },
     {
         country:"USA",
         code:"US",
