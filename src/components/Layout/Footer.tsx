9345ea46ddd971d8cd1351742028a50a958bda0f
saadman.sayeed <saadman.sayeed@bjitgroup.com>
2024-11-20 15:40:50 +0600

    Added info

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -9,8 +9,8 @@ export default function Footer({}: Props) {
       {/* Footer */}
       <footer className="bg-blue-600 text-white py-4 text-center">
         <p>&copy; {new Date().getFullYear()} {siteInfo.siteTitle}</p>
-        <div>
-          <p className="font-bolder">Framework developed by: Chowdhury Shahriar Muzammel</p>
+        <div className="p-1 rounded-sm shadow-lg ">
+          <p className="font-semibold">Framework developed by: Chowdhury Shahriar Muzammel</p>
           <p>PhD Researcher (Computer Science), RMIT University, Australia</p>
           <p>Contact: s3987367@student.rmit.edu.au</p>
         </div>
