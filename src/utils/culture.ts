fa19f01b86e502aeb1bd15672b23afd2e06022ff
AnikCSE008 <shahriar@cou.ac.bd>
2024-11-24 22:23:28 +1100

    Update culture.ts

--- src/utils/culture.ts
+++ src/utils/culture.ts
@@ -147,7 +147,8 @@ export const cultureData:Array<cultureDataType> =[
         country:"Australia",
         code:"AU",
         culture:[
-            { key: "Openness and Honesty", value: "High", description:"Openness is about sharing ideas freely, while honesty means being truthful." },
+            { key: "Openness and Honesty", value: "High", description:"Openness is about sharing ideas freely, while honesty means being truthful. In individualistic cultures like the US, people are direct and open, valuing clear communication.
+In collectivist cultures like China, people may express honesty more subtly to maintain harmony." },
             { key: "Gender preference", value: "High" },
             { key: "Aiming for quick results", value: "Moderate" },
             { key: "Face saving", value: "Moderate" },
