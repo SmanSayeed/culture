cc21eacda2e4894bbda7ce2608165242aab40e10
AnikCSE008 <shahriar@cou.ac.bd>
2024-12-30 13:09:01 +1100

    Update Footer.tsx

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -10,7 +10,7 @@ export default function Footer({}: Props) {
       <footer className="bg-blue-600 text-white py-4 text-center flex justify-center items-center flex-col">
         <p>&copy; {new Date().getFullYear()} {siteInfo.siteTitle}</p>
         <div className="p-1 rounded-md shadow-lg bg-blue-800 my-3">
-          <p className="font-semibold">Framework developed by: Chowdhury Shahriar Muzammel, Maria Spichkova, James Harland</p>
+          <p className="font-semibold">Framework developed by: Chowdhury Shahriar Muzammel, Dr. Maria Spichkova, Dr. James Harland</p>
           <p>School of Computing Technologies, RMIT University, Australia</p>
           <p>Contact: s3987367@student.rmit.edu.au</p>
         </div>
