3dd58f056b0990f649c80a38849425429f9e7450
AnikCSE008 <shahriar@cou.ac.bd>
2024-12-31 13:36:04 +1100

    Update culture.ts

--- src/utils/culture.ts
+++ src/utils/culture.ts
@@ -56,9 +56,9 @@ export const cultureData:Array<cultureDataType> =[
     },
 
     {
-    "country": "Albania",
-    "code": "AL",
-    "culture": [
+     country: "Albania",
+     code: "AL",
+     culture: [
         { key: "Centralised decision-making", value: "High", description: "Centralized decision-making refers to a process where decisions about requirements, priorities, and solutions are made by a single authority or a small group of individuals, rather than being distributed among multiple stakeholders." },
         { key: "Managers’ influence", value: "High", description: "Managers' influence refers to the impact that managerial decisions, priorities, and authority have on shaping the direction, scope, and implementation of requirements." },
         { key: "Deference", value: "High", description: "Deference refers to showing respect or yielding to the opinions, decisions, or authority of others, often influencing how stakeholders interact, collaborate, and make decisions within the process." },
