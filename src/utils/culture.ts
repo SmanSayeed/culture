c5736735147649aa7a73f5518a60b54cf4b569a6
AnikCSE008 <shahriar@cou.ac.bd>
2024-12-31 13:47:30 +1100

    Update culture.ts

--- src/utils/culture.ts
+++ src/utils/culture.ts
@@ -197,9 +197,10 @@ export const cultureData:Array<cultureDataType> =[
     ]
 },
 
+    
     {
-    country: "Croatia",
-    code: "HR",
+    country: "El Salvador",
+    code: "SV",
     culture: [
         { key: "Centralised decision-making", value: "High", description: "Centralized decision-making refers to a process where decisions about requirements, priorities, and solutions are made by a single authority or a small group of individuals, rather than being distributed among multiple stakeholders." },
         { key: "Managers’ influence", value: "High", description: "Managers' influence refers to the impact that managerial decisions, priorities, and authority have on shaping the direction, scope, and implementation of requirements." },
