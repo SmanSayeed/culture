b2b9ac91ef0e9cd40b799d612205d8ccaee2fbe0
AnikCSE008 <shahriar@cou.ac.bd>
2025-01-01 14:20:23 +1100

    Update culture.ts

--- src/utils/culture.ts
+++ src/utils/culture.ts
@@ -2006,9 +2006,49 @@ export const cultureData:Array<cultureDataType> =[
     ]
 },
 
-    
 
-    
+    {
+    country: "Hong Kong",
+    code: "HK",
+    culture: [
+        { key: "Centralised decision-making", value: "High", description: "Centralized decision-making refers to a process where decisions about requirements, priorities, and solutions are made by a single authority or a small group of individuals, rather than being distributed among multiple stakeholders." },
+        { key: "Managers’ influence", value: "High", description: "Managers' influence refers to the impact that managerial decisions, priorities, and authority have on shaping the direction, scope, and implementation of requirements." },
+        { key: "Deference", value: "High", description: "Deference refers to showing respect or yielding to the opinions, decisions, or authority of others, often influencing how stakeholders interact, collaborate, and make decisions within the process." },
+        { key: "Establish trust", value: "High", description: "Establish trust refers to building confidence among stakeholders through transparent communication, reliability, and consistent actions, which fosters cooperation and effective collaboration throughout the process." },
+        { key: "Building relationships", value: "High", description: "Building relationships involves establishing trust, open communication, and mutual respect among stakeholders, which fosters collaboration, facilitates the gathering of accurate requirements, and supports long-term project success." },
+        { key: "Aiming for quick results", value: "High", description: "Aiming for quick results reflects a cultural preference for immediate achievements and efficiency over long-term planning and gradual progress." },
+        { key: "Empathy with users", value: "High", description: "Empathy with users involves understanding and sharing the feelings, needs, and perspectives of end users, ensuring that the requirements reflect their real-world experiences and challenges." },
+        { key: "Recognition of uncertainty", value: "High", description: "Recognition of uncertainty involves acknowledging and addressing unknowns, ambiguities, or risks in the requirements process, allowing for flexibility and adaptability in decision-making and planning." },
+        { key: "Subordinate avoid taking risks", value: "High", description: "Subordinates avoiding taking risks means that lower-level team members may hesitate to make decisions or propose new ideas due to fear of making mistakes or facing negative consequences, often relying on higher authority for guidance." },
+        { key: "Communication context", value: "Moderate", description: "Communication context refers to the surrounding factors, such as cultural norms, language, interpretation, and understanding among stakeholders." },
+        { key: "Gender preference", value: "Moderate", description: "Gender Preference reflects cultural priorities between competitive, performance-driven traits and collaborative, user-centered values." },
+        { key: "Solving conflicts by favoritism", value: "Moderate", description: "Solving conflicts by favoritism involves resolving disagreements by prioritizing the interests or preferences of certain stakeholders over others, potentially undermining fairness and collaboration in the decision-making process." },
+        { key: "Avoiding conflicts", value: "Moderate", description: "Avoiding conflicts means trying to prevent disagreements by avoiding issues that could cause tension or problems among stakeholders." },
+        { key: "Hierarchical structure", value: "Low", description: "Hierarchical structure refers to an organizational setup where decision-making authority is clearly defined in levels, with higher-level managers having more control over requirements and decisions than lower-level team members." },
+        { key: "Face saving", value: "Low", description: "Face saving reflects a cultural emphasis on maintaining respect and avoiding embarrassment, influencing how feedback is given, decisions are communicated, and conflicts are managed." },
+        { key: "Punctuality", value: "Low", description: "Punctuality refers to the importance of meeting deadlines and being on time for meetings and deliverables, ensuring that the project progresses smoothly and stakeholder expectations are met." },
+        { key: "Solution-focused requirements", value: "Low", description: "Solution-focused requirements emphasize defining needs based on predefined solutions, often prioritizing implementation details over exploring underlying problems or user needs." },
+        { key: "Employees’ attitude", value: "Low", description: "Employees’ attitude is the collective mindset and behavior of employees in the workplace, shaped by cultural values, perceptions, and interactions with organizational structures." }
+    ]
+},
+
+    {
+    country: "Hungary",
+    code: "HU",
+    culture: [
+        { key: "Clients’ resistance", value: "High", description: "Clients' resistance refers to the reluctance or opposition from clients to accept proposed requirements, changes, or solutions, often due to concerns about cost, usability, or alignment with their needs or expectations." },
+        { key: "Subordinate avoid taking risks", value: "High", description: "Subordinates avoiding taking risks means that lower-level team members may hesitate to make decisions or propose new ideas due to fear of making mistakes or facing negative consequences, often relying on higher authority for guidance." },
+        { key: "Aiming for quick results", value: "High", description: "Aiming for quick results means focusing on fast decision-making and rapid delivery of requirements or solutions, often prioritizing speed over thorough analysis or long-term planning." },
+        { key: "Dress code", value: "High", description: "Dress Code refers to the expected or preferred style of clothing in the workplace or during meetings, which can influence professionalism, communication, and how stakeholders perceive each other during the requirements gathering process." },
+        { key: "Openness and Honesty", value: "Moderate", description: "Openness is about sharing ideas freely, while honesty means being truthful. In individualistic cultures like the US, people are direct and open, valuing clear communication. In collectivist cultures like China, people may express honesty more subtly to maintain harmony." },
+        { key: "Gender preference", value: "Moderate", description: "Gender Preference reflects cultural priorities between competitive, performance-driven traits and collaborative, user-centered values." },
+        { key: "Take responsibility", value: "Moderate", description: "Take responsibility reflects a cultural tendency to prioritize accountability and ownership in decision-making and task completion." },
+        { key: "Hidden agenda", value: "Moderate", description: "Hidden agenda refers to the presence of unspoken motives or goals that influence stakeholder interactions and decision-making, potentially impacting transparency and collaboration." },
+        { key: "Relying on previous projects", value: "Moderate", description: "Relying on previous projects means using lessons, templates, or solutions from past projects as a basis for making decisions and defining requirements in current projects, to save time and reduce uncertainty." },
+        { key: "Belief in expertise", value: "Low", description: "Belief in expertise refers to the reliance on the knowledge, skills, and experience of specialists or experts to guide decision-making and shape requirements, often leading to a greater trust in their input and recommendations." }
+    ]
+},
+
     
 
     
